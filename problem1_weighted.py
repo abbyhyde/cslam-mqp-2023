d1ee4f56d3d64d5d65191dcacf150f51f00591d6
ngrumski <grumski88@gmail.com>
2022-10-09 12:40:00 -0400

    fixing bug is weighted that allows more memory to be utilized

--- problem1_weighted.py
+++ problem1_weighted.py
@@ -1,28 +1,10 @@
 import random, numpy, math
 
-def isTravelable(path):
-  nodes_visit = []
-  nodes_reachable = []
-  for i in range(nodes):
-        if (math.floor(path[1] / ( 2**i )) % 2 == 1):
-          nodes_visit.append(i)
-  if nodes_visit[0] != 0:
-      return False
-  else:
-    nodes_reachable.append(nodes_visit[0])
-  for curr_node in nodes_reachable:
-    for test_node in nodes_visit:
-      if adj_grid[curr_node][test_node] == 1 and test_node not in nodes_reachable:
-        nodes_reachable.append(test_node)
-  if len(nodes_reachable) == len(nodes_visit):
-    return True
-  return False
-
 def calculateNodeWeight(node_weights, index):
-    total = 0
-    for node_weight in node_weights:
-        total += node_weight
-    return node_weights[index]/total
+  total = 0
+  for node_weight in node_weights:
+    total += node_weight
+  return node_weights[index]/total
 
 
 nodes = 10
@@ -45,36 +27,31 @@ print(node_memory)
 # mapping the path
 at_end = False
 memory_left = robot_memory
-next_node = 0
-current_node = 0
-nodes_visited = []
-possible_next_nodes = []
-pick = 0
-nodes_visited.append(current_node)
+nodes_visited = [0]
 # while not at the end or no more possible options
 while (not at_end):
+  possible_next_nodes = []
   # can only consider a node if there's an edge and hasn't been visited yet and has a smaller memory cost
-  for i in range(0,nodes):
-    if((adj_grid[current_node][i] == 1) and (i not in nodes_visited) and (current_node != i)):
-      # add number to array
-      possible_next_nodes.append(i)
+  for j in nodes_visited:
+    for i in range(0,nodes):
+      if((adj_grid[j][i] == 1) and (i not in nodes_visited) and (j != i) and node_memory[i] <= memory_left):
+        # add number to array
+        possible_next_nodes.append(i)
   
-  # pick next node
-  choice = random.random()
-  next_node = -1
-  while choice >= 0:
-    next_node += 1
-    choice -= calculateNodeWeight(node_memory, next_node)
+
   
   # if robot has enough memory to go to the selected node and robot has not visited it already
-  if (node_memory[next_node] < memory_left and next_node not in nodes_visited):
-    nodes_visited.append(next_node)
-    current_node = next_node
-    memory_left -= node_memory[next_node]
-    print("Next node: " + str(next_node) + ", has cost of " + str(node_memory[next_node]))
+  if (len(possible_next_nodes) > 0):
+    # pick next node
+    choice = random.random() - calculateNodeWeight(node_memory, possible_next_nodes[0])
+    next_node_index = 0
+    while choice >= 0 and next_node_index < len(possible_next_nodes)-1:
+      next_node_index = 1 + next_node_index
+      choice -= calculateNodeWeight(node_memory, possible_next_nodes[next_node_index])
+    nodes_visited.append(possible_next_nodes[next_node_index])
+    memory_left -= node_memory[possible_next_nodes[next_node_index]]
+    print("Next node: " + str(possible_next_nodes[next_node_index]) + ", has cost of " + str(node_memory[possible_next_nodes[next_node_index]]))
     print("Available memory left: " + str(memory_left))
   else:
     at_end = True
-    print("Done with path at node " + str(current_node))
-    print(adj_grid[current_node])
     print("Path: " + str(nodes_visited))
