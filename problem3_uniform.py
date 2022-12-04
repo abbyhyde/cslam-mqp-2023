5eb1b85c44afbb2c55680c0d6090b8550e061eac
Abby Hyde <abbyghyde@gmail.com>
2022-12-03 21:20:08 -0500

    added robot handler and parallelism (mostly)

--- problem3_uniform.py
+++ problem3_uniform.py
@@ -1,5 +1,16 @@
-import random, numpy, math, time
-import params
+import random, numpy, math, time, logging, threading
+import params, robot_handler
+
+threads = []
+
+#settings params from params.py
+nodes = params.nodes
+robot_memory = params.memory
+robots = params.robots
+random.seed(params.seed)
+
+nodes_mapped = numpy.empty(nodes)
+adj_grid, node_memory = robot_handler.generate()
 
 def isTravelable(path):
   nodes_visit = []
@@ -19,129 +30,99 @@ def isTravelable(path):
     return True
   return False
 
-#settings params from params.py
-nodes = params.nodes
-robot_memory = params.memory
-robots = params.robots
-random.seed(params.seed)
-
-for i in range(0,1):  # the number of times we're running the experiment
-  adj_grid = numpy.eye(nodes, nodes, 0, int)
-  node_memory = numpy.empty(nodes)
-  nodes_mapped = numpy.empty(nodes)
-  prob_connection = 0.5
-  for i in range(0,nodes): #assumes the starting node is only connected to the first node add the ability for other nodes to be reached from the start
-    rfloat = random.random()
-    node_memory[i] = (robot_memory/1.5)*rfloat
-    for j in range(i,nodes):
-      if(i == j or random.random() < prob_connection):
-        adj_grid[i][j] = 1
-        adj_grid[j][i] = 1
-  # print(adj_grid)
-  # print(node_memory)
-
-  # perform bfs to check whether the graph is connected
-  current_node_index = 0
-  nodes_checked = []
-  nodes_checked.append(current_node_index)
-  queue = []
-  queue.append(current_node_index)
-  while queue:
-    current_node_index = queue.pop(0)
-    # print(current_node_index)
-    for i in adj_grid[current_node_index]:
-      if i not in nodes_checked and adj_grid[current_node_index][i] == 1:
-        nodes_checked.append(i)
-        queue.append(i)
-  
-  # now we check which nodes were not covered by bfs
-  for j in range(nodes):
-    if j not in nodes_checked:
-      # connect the node to a random node in the main part of the graph
-      connection_index = math.floor(random.random() * len(nodes_checked))
-      adj_grid[connection_index][j] = 1
-      adj_grid[j][connection_index] = 1
-      # print("connected " + str(j) + " to " + str(connection_index))
-
+def uniform_alg(index, threshold_ct):
+  # will contain the entire uniform alg
   # uniform probabilistic algorithm
   # gives each adj node an equal chance and picks one
   nodes_mapped[0] = 1
   # while not at the end or no more possible options
   start = time.monotonic_ns()
-  def isDone():
-    for i in nodes_mapped:
-        if(i != 1):
-            return True
-    return False
+  # def isDone():
+  #   for i in nodes_mapped:
+  #       if(i != 1):
+  #           return True
+  #   return False
 
-  robot = 0
-  while(isDone()):
-    # mapping the path
-    at_end = False
-    expand = False
-    highest_cost = 0
-    memory_left = robot_memory
-    next_node = 0
-    current_node = 0
-    nodes_visited = []
+  # while(isDone()):
+  # print("going back to prev while loop")
+  # mapping the path
+  at_end = False
+  expand = False
+  highest_cost = 0
+  memory_left = robot_memory
+  next_node = 0
+  current_node = 0
+  nodes_visited = []
+  possible_next_nodes = []
+  pick = 0
+  nodes_visited.append(current_node)
+  # while not at the end or no more possible options
+  start = time.monotonic_ns()
+  while (not at_end):
+    # print("Robot " + str(index) + " starting loop " + str(at_end))
     possible_next_nodes = []
-    pick = 0
-    nodes_visited.append(current_node)
-    # while not at the end or no more possible options
-    start = time.monotonic_ns()
-    while (not at_end):
-      possible_next_nodes = []
-      # can only consider a node if there's an edge and hasn't been visited yet and has a smaller memory cost
-      for j in nodes_visited:
-        for i in range(0,nodes):
-          if((adj_grid[j][i] == 1) and (i not in nodes_visited) and (j != i) and node_memory[i] <= memory_left and (i not in possible_next_nodes)):
-            # add number to array
-            possible_next_nodes.append(i)
-      # print(nodes_visited)
+    # can only consider a node if there's an edge and hasn't been visited yet and has a smaller memory cost
+    for j in nodes_visited:
+      for i in range(0,nodes):
+        if((adj_grid[j][i] == 1) and (i not in nodes_visited) and (j != i) and node_memory[i] <= memory_left and (i not in possible_next_nodes)):
+          # add number to array
+          possible_next_nodes.append(i)
+    # print(nodes_visited)
+    # print(nodes_mapped)
+    # print(possible_next_nodes)
+    
+    # if robot has enough memory to go to the selected node and robot has not visited it already
+    """
+    if there are possible next nodes:
+      while there are possible next nodes
+        randomly pick one
+        if the node memory is not the highest cost
+          make it the next node
+        else
+          remove from possible next nodes list
+      here if ran out of nodes
+      pick one from the visited nodes and make it the next node
+    """
+    size = len(possible_next_nodes)
+    if (size > 0):
+      # print("Robot " + str(index) + " where we want")
+      while(size > 0):
+        next_node = possible_next_nodes[math.floor(random.random()*size)]
+        # print(str(size) + "   " + str(next_node))
+        if(highest_cost < node_memory[next_node] and nodes_mapped[next_node] != 1):
+          highest_cost = node_memory[next_node]
+          break
+        else:    
+          # print("removing node " + str(next_node))
+          possible_next_nodes.remove(next_node)
+          size = len(possible_next_nodes)
+      nodes_visited.append(next_node)
+      nodes_mapped[next_node] = 1
+      memory_left -= node_memory[next_node]
+      highest_cost = 0
+      if (threshold_ct == index): 
+        threshold_ct += 1
+      # print("remaining memory: " + str(memory_left))
+    elif(not expand):
+      # print("Robot " + str(index) + " again this is fine")
+      expand = True
+      for i in range(1, len(nodes_mapped)):
+          if (i not in nodes_visited and nodes_mapped[i] == 1):
+              nodes_visited.append(i)
+              if (threshold_ct == index): 
+                threshold_ct += 1
+    else:
+      at_end = True
+      # print("robot-" + str(robot)+ " " + str(robot_memory - memory_left)+ " " + str((time.monotonic_ns()-start)/1000000))
+      print("Robot " + str(index) + ": " + str(robot_memory - memory_left))
       # print(nodes_mapped)
-      # print(possible_next_nodes)
-      
-      # if robot has enough memory to go to the selected node and robot has not visited it already
-      """
-      if there are possible next nodes:
-        while there are possible next nodes
-          randomly pick one
-          if the node memory is not the highest cost
-            make it the next node
-          else
-            remove from possible next nodes list
-        here if ran out of nodes
-        pick one from the visited nodes and make it the next node
-      """
-      size = len(possible_next_nodes)
-      if (size > 0):
-        while(size > 0):
-          next_node = possible_next_nodes[math.floor(random.random()*size)]
-          # print(str(size) + "   " + str(next_node))
-          if(highest_cost < node_memory[next_node] and nodes_mapped[next_node] != 1):
-            highest_cost = node_memory[next_node]
-            break
-          else:    
-            # print("removing node " + str(next_node))
-            possible_next_nodes.remove(next_node)
-            size = len(possible_next_nodes)
-        nodes_visited.append(next_node)
-        nodes_mapped[next_node] = 1
-        memory_left -= node_memory[next_node]
-        highest_cost = 0
-        # print("remaining memory: " + str(memory_left))
-      elif(not expand):
-        expand = True
-        for i in range(1, len(nodes_mapped)):
-            if (i not in nodes_visited and nodes_mapped[i] == 1):
-                nodes_visited.append(i)
-      else:
-        at_end = True
-        print("robot-" + str(robot)+ " " + str(robot_memory - memory_left)+ " " + str((time.monotonic_ns()-start)/1000000))
-        print(nodes_mapped)
-        robot += 1
-        # print("Not enough memory left, done with path at node " + str(current_node))
-        # print("Available memory left: " + str(memory_left))
-        # print(adj_grid[current_node])
-        print("Path: " + str(nodes_visited))
-        input()
+      # print("Not enough memory left, done with path at node " + str(current_node))
+      # print("Available memory left: " + str(memory_left))
+      # print(adj_grid[current_node])
+      print("Robot " + str(index) + "'s path: " + str(nodes_visited))
+        # input()
+  # print("      Robot " + str(index) + " done")
+
+robot_handler.run_next_robot(uniform_alg)
+
+# robot_handler.join_robots(threads)
---
+++ robot_handler.py
@@ -0,0 +1,70 @@
+""" 
+Generates graph for each runthrough as well as manages the thread for each robot. 
+"""
+import random, numpy, math, time, logging, threading
+import params
+
+#settings params from params.py
+nodes = params.nodes
+robot_memory = params.memory
+robots = params.robots
+random.seed(params.seed)
+
+def generate():
+    adj_grid = numpy.eye(nodes, nodes, 0, int)
+    node_memory = numpy.empty(nodes)
+    prob_connection = 0.5
+    for i in range(0,nodes): #assumes the starting node is only connected to the first node add the ability for other nodes to be reached from the start
+        rfloat = random.random()
+        node_memory[i] = (robot_memory/1.5)*rfloat
+        for j in range(i,nodes):
+            if(i == j or random.random() < prob_connection):
+                adj_grid[i][j] = 1
+                adj_grid[j][i] = 1
+    # print(adj_grid)
+    # print(node_memory)
+
+    # perform bfs to check whether the graph is connected
+    current_node_index = 0
+    nodes_checked = []
+    nodes_checked.append(current_node_index)
+    queue = []
+    queue.append(current_node_index)
+    while queue:
+        current_node_index = queue.pop(0)
+        # print(current_node_index)
+        for i in adj_grid[current_node_index]:
+            if i not in nodes_checked and adj_grid[current_node_index][i] == 1:
+                nodes_checked.append(i)
+                queue.append(i)
+    
+    # now we check which nodes were not covered by bfs
+    for j in range(nodes):
+        if j not in nodes_checked:
+            # connect the node to a random node in the main part of the graph
+            connection_index = math.floor(random.random() * len(nodes_checked))
+            adj_grid[connection_index][j] = 1
+            adj_grid[j][connection_index] = 1
+            # print("connected " + str(j) + " to " + str(connection_index))
+    return adj_grid, node_memory
+
+def run_next_robot(algorithm):
+    # start robots
+    threads = []
+    # join robot threads to make sure they complete
+
+    # runs all robots
+    index = 0
+    threshold_ct = 0
+    while (index < robots):
+        print("     Main: create and start robot " + str(index))
+        new_thread = threading.Thread(target=algorithm, args=(index,threshold_ct,))
+        new_thread.start()
+        threads.append(new_thread)
+        index += 1
+        # print(str(threshold_ct) + " " + str(index))
+        # while (threshold_ct < count):
+        #   time.sleep(0.1)
+    for index, thread in enumerate(threads):
+        thread.join()
+        print("     Main: done with robot " + str(index))
