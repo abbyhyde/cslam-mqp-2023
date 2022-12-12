802163e698fa51c6568a3b077006553666515051
ngrumski <grumski88@gmail.com>
2022-12-12 10:32:49 -0500

    blocked out robot class

--- robot_handler.py
+++ robot_handler.py
@@ -72,3 +72,46 @@ def run_next_robot(algorithm,signal,doneSignal):
     for index, thread in enumerate(threads):
         thread.join()
         print("     Main: done with robot " + str(index))
+
+
+class Robot:
+    def __init__(self, id, alg, max_memory):
+        self.id = id
+        self.alg = alg
+        self.max_memory = max_memory
+        self.memory_usage = 0
+        self.nodes_to_visit = []
+        self.nodes_visited = []
+        self.memory_left_to_map = 0
+        self.edge_tracker = dict()
+        self.curr_node = -1 # -1 means outside the graph, robots always enter into node 0
+        
+    def act(self): # warning does not add new nodes it discovers yet if they can fit
+        if(self.curr_node == -1 and len(self.nodes_to_visit) == 0): #if the robot is at the base with nothing to do
+            return True #returns the true that it wants to be included in the auction
+        elif(self.memory_left_to_map > 0): #if the robot is still mapping a node
+            self.memory_left_to_map -= 1
+            return False #does not want to be included in the auction
+        else: #robot moves
+            if(self.curr_node == -1): #move onto the graph (node 0)
+                #if node isnt mapped add set it's memory to memory left to map
+                self.curr_node = 0
+                return False
+            escape_edge = None 
+            valid_edges = list(self.edge_tracker)
+            for edge in valid_edges: #trying to find a new edge to traverse before using an old edge. dont use edges more than twice
+                if(self.curr_node in edge):
+                    value = self.edge_tracker[edge]
+                    if(value == 0): #if an edge is new we traverse it
+                        #if node isnt mapped add set it's memory to memory left to map
+                        self.curr_node = edge[0] if self.curr_node == edge[1] else edge[1]
+                        edge_tracker[edge] = 1 #mark that we traversed it
+                        return False
+                    elif(value == 1):
+                        escape_edge = edge
+            self.curr_node = escape_edge[0] if self.curr_node == escape_edge[1] else escape_edge[1] # if no new edges, use the escape edge
+            return False
+
+    def pick(self):
+        self.nodes_to_visit, result = self.algorithm(adj_grid, node_memory, self.nodes_to_visit, self.max_memory)
+        return result, self.nodes_to_visit
