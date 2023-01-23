5780965b20f87bebb7fcd1e34ba05e9e23397ed1
Abby Hyde <abbyghyde@gmail.com>
2023-01-22 22:53:42 -0500

    created jmst for problem 6

--- problem6_greedy.py
+++ problem6_greedy.py
@@ -8,7 +8,7 @@ def greedy_alg(adj_grid, node_memory, nodes_to_visit, max_memory, nodes):
     greatest_node_index = -1
     memory_to_map = 0
     for node_index in nodes_to_visit:
-        if(nodes[node_index] != robot_handler.Robot_State.MAPPED): # change this is you want to count previouslty mapped nodes
+        if(nodes[node_index] != robot_handler.Robot_State.MAPPED): # change this if you want to count previously mapped nodes
             memory_to_map += node_memory[node_index]
     for node_index in range(len(node_memory)):
         if(nodes[node_index] == robot_handler.Robot_State.NOT_CLAIMED and memory_to_map + node_memory[node_index] <= max_memory
