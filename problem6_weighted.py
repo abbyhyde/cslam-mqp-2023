52d04eb31fdff71b874507848f6ee223a3f430cc
ngrumski <grumski88@gmail.com>
2023-01-24 21:40:03 -0500

    weighted alg

--- problem6_weighted.py
+++ problem6_weighted.py
@@ -22,29 +22,24 @@ def weighted_alg(adj_grid, node_memory, nodes_to_visit, max_memory, nodes):
         if(nodes[node_index] != robot_handler.Robot_State.MAPPED): # change this if you want to count previously mapped nodes
             memory_to_map += node_memory[node_index]
 
-    # calculate next node based on randomly picking one and checking if it meets qualifications  
-    # nodes_checked = []
-    # node_found = False
-    # while (not node_found):
-    #     next_node_index = random.random() - calculateNodeWeight(node_memory, nodes_to_visit[0])
-    #     # print(next_node_index)
-    #     if (len(nodes_checked) == len(nodes_to_visit)):
-    #         # print("out")
-    #         return None, True
-    #     elif (nodes[next_node_index] == robot_handler.Robot_State.NOT_CLAIMED 
-    #               and memory_to_map + node_memory[next_node_index] <= max_memory):
-    #         # print("ok")
-    #         node_found = True
-    #         return next_node_index, False
-    #     elif (next_node_index not in nodes_checked):
-    #         # print("added " + str(next_node_index))
-    #         nodes_checked.append(next_node_index)
-  
-    if(next_node_index >= 0):
-        print(next_node_index)
-        return next_node_index, False
+    possible_nodes = []
+    for node_index in range(len(nodes)):
+        if (nodes[node_index] == robot_handler.Robot_State.NOT_CLAIMED and memory_to_map + node_memory[node_index] <= max_memory):
+            possible_nodes.append(node_index)
+
+    if(len(possible_nodes) > 0):
+      choice = random.random() - calculateNodeWeight(node_memory, possible_nodes[0]) # randomize how much weight we'll go through in order
+      next_node_index = 0
+      next_node = possible_nodes[next_node_index]
+      while choice >= 0 and next_node_index < len(possible_nodes)-1: # while there's still stuff to go through
+        next_node_index = 1 + next_node_index
+        next_node = possible_nodes[next_node_index]
+        choice -= calculateNodeWeight(node_memory, next_node) # remove the next amount of stuff to go through
+      return next_node, False
     else:
-        return None, True
+      return None, True
+
+    
 
 robot_handler.generate()
 robot_handler.run_all_robots(weighted_alg)
