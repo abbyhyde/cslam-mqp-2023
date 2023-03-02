d07c3ad3f0ecd0d86e773e1172b83c1caea1a4dd
Zachary Rivernder <zarivernider@wpi.edu>
2023-03-02 18:30:08 -0500

    finished data collection

--- problem6_variedgraphcollection.py
+++ problem6_variedgraphcollection.py
@@ -32,12 +32,12 @@ speed_rounds = [[]]
 x_axis = []
 
 # sum of node memory is a certain ratio of the amount of memory of all the robots, 50-200%
-graph_type = 0 # 0 for lattice, 1 for fully connected, 2 for tree
-num_trials = 5
+graph_type = 2 # 0 for lattice, 1 for fully connected, 2 for tree
+num_trials = 30
 
 # runs robot handler and collects data along the way
-def run_robots(a):
-    if (a > 0):
+def run_robots(a, b):
+    if (a > 0 and b == 0):
         greedy_rounds.append([])
         greedy_usage.append([])
         jmst_rounds.append([])
@@ -48,29 +48,41 @@ def run_robots(a):
         weighted_usage.append([])
         speed_rounds.append([])
 
-    for i in range(num_trials):
+    # for i in range(num_trials):
         # running robots returns num rounds and memory usage
-        # print("trial #" + str(i))
-        greedy_trials.append(robot_handler.run_all_robots(problem6_greedy.greedy_alg))
-        jmst_trials.append(robot_handler.run_all_robots(problem6_jmst.jmst_alg))
-        uniform_trials.append(robot_handler.run_all_robots(problem6_uniform.uniform_alg))
-        weighted_trials.append(robot_handler.run_all_robots(problem6_weighted.weighted_alg))
-        speed_trials.append(robot_handler.run_all_robots(problem6_speed.speedy_alg))
-    
-    print("processing data...")
+    # print("trial #" + str(b))
+    greedy_trials.append(robot_handler.run_all_robots(problem6_greedy.greedy_alg,b))
+    jmst_trials.append(robot_handler.run_all_robots(problem6_jmst.jmst_alg,b))
+    speed_trials.append(robot_handler.run_all_robots(problem6_speed.speedy_alg,b))
+
+    uniform_current = []
+    weighted_current = []
+    uniform_sum = 0
+    weighted_sum = 0
+    uniform_avg_usage = numpy.zeros(params.num_nodes)
+    weighted_avg_usage = numpy.zeros(params.num_nodes)
+    uniform_new = [] 
+    weighted_new = []
     for i in range(num_trials):
-        greedy_rounds[a].append(greedy_trials[(a*num_trials)+i][0])
-        greedy_usage[a].extend(greedy_trials[(a*num_trials)+i][1])
-        speed_rounds[a].append(speed_trials[(a*num_trials)+i][0])
-        jmst_rounds[a].append(jmst_trials[(a*num_trials)+i][0])
-        jmst_usage[a].extend(jmst_trials[(a*num_trials)+i][1])
-        uniform_rounds[a].append(uniform_trials[(a*num_trials)+i][0])
-        uniform_usage[a].extend(uniform_trials[(a*num_trials)+i][1])
-        weighted_rounds[a].append(weighted_trials[(a*num_trials)+i][0])
-        weighted_usage[a].extend(weighted_trials[(a*num_trials)+i][1])
-        x_axis.append(numpy.array(params.node_mem_pct))
-    
-    print("done with runs")
+        uniform_current.append(robot_handler.run_all_robots(problem6_uniform.uniform_alg,b))
+        weighted_current.append(robot_handler.run_all_robots(problem6_weighted.weighted_alg,b))
+    for j in range(num_trials):
+        # print(len(uniform_current[j][1]))
+        uniform_sum += uniform_current[j][0]
+        weighted_sum += weighted_current[j][0]
+        # for k in range(len(uniform_current[j][1])):
+        #     # print(str(j) + " " + str(k))
+        #     uniform_avg_usage[k] += uniform_current[j][1][k]
+        #     weighted_avg_usage[k] += weighted_current[j][1][k]
+    uniform_avg = uniform_sum/num_trials
+    weighted_avg = weighted_sum/num_trials
+    # print(str(uniform_avg) + " " + str(weighted_avg))
+    uniform_new.append(uniform_avg)
+    # uniform_new.append(uniform_avg_usage)
+    weighted_new.append(weighted_avg)
+    # weighted_new.append(weighted_avg_usage)
+    uniform_trials.append(uniform_new)
+    weighted_trials.append(weighted_new)
 
 # generates graphs with data collected
 def analyze_data():
@@ -91,9 +103,27 @@ def analyze_data():
 
 def main():
     for a in range(4):
-        params.node_mem_pct = (a+1)*0.5
-        robot_handler.generate_graph(graph_type, params.node_mem_pct)
-        run_robots(a)
+        for b in range(num_trials):
+            print("trial #" + str(b))
+            params.node_mem_pct = (a+1)*0.5
+            robot_handler.generate_graph(graph_type, params.node_mem_pct)
+            params.seed += 1
+            run_robots(a, b)
+
+        print("processing data...")
+        for i in range(num_trials):
+            greedy_rounds[a].append(greedy_trials[(a*num_trials)+i][0])
+            greedy_usage[a].extend(greedy_trials[(a*num_trials)+i][1])
+            speed_rounds[a].append(speed_trials[(a*num_trials)+i][0])
+            jmst_rounds[a].append(jmst_trials[(a*num_trials)+i][0])
+            jmst_usage[a].extend(jmst_trials[(a*num_trials)+i][1])
+            uniform_rounds[a].append(uniform_trials[(a*num_trials)+i][0])
+            # uniform_usage[a].extend(uniform_trials[(a*num_trials)+i][1])
+            weighted_rounds[a].append(weighted_trials[(a*num_trials)+i][0])
+            # weighted_usage[a].extend(weighted_trials[(a*num_trials)+i][1])
+            x_axis.append(numpy.array(params.node_mem_pct))
+        print("done with runs")
+        params.seed = 204
     analyze_data()
 
 if __name__ == "__main__":
